fa370afa00ae5d1e2f973cbf31a163d23e36127d
Tim Moyence <tim.moyence@gmail.com>
2023-12-04 10:27:00 +0100

    :sparkles: :construction: create form with instant modification and no persistante

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -7,10 +7,13 @@ import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 import { RouterModule, Routes } from '@angular/router';
 import { PokemonService } from './pokemon.service';
 import { FormsModule } from '@angular/forms';
+import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
+import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
 
 const pokemonRoutes: Routes = [
   { path: 'pokemons', component: ListPokemonComponent},
   { path: 'pokemon/:id' , component: DetailPokemonComponent},
+  { path: 'pokemon/edit/:id', component: EditPokemonComponent}
 ];
 
 @NgModule({
@@ -18,7 +21,9 @@ const pokemonRoutes: Routes = [
     ListPokemonComponent,
     DetailPokemonComponent,
     BorderCardDirective,
-    PokemonTypeColorPipe
+    PokemonTypeColorPipe,
+    PokemonFormComponent,
+    EditPokemonComponent
   ],
   imports: [
     CommonModule,
